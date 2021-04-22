8f15ea2dbf17ea3e81d4ae2975dda9ad49977d84
Jose Chavarría <chavarria.j4r@gmail.com>
2021-04-22 02:02:19 -0600

    added create function in repositories

--- src/components/Todo/domain/todo.repository.ts
+++ src/components/Todo/domain/todo.repository.ts
@@ -2,4 +2,5 @@ import TodoEntity from './todo.entity'
 
 export interface TodoRepository  {
     findAll(): Promise<TodoEntity[]>
+    createOne(body: TodoEntity): Promise<TodoEntity>
 }
--- src/components/Todo/infrastructure/todo.mongo.repository.ts
+++ src/components/Todo/infrastructure/todo.mongo.repository.ts
@@ -3,6 +3,10 @@ import { TodoRepository } from "../domain/todo.repository";
 import Todos, { ITodo } from "./todo.datasource";
 
 export class TodoMongoRepository implements TodoRepository {
+    public async createOne(body: TodoEntity) {
+        const result: ITodo = await Todos.create(body)
+        return result
+    }
     public async findAll() {
         // Infra conditions
         const result: ITodo[] = await Todos.find() 
